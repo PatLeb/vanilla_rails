de9ff6452e93e245bedc8f13f177be3a4ef1ca40
Patrice Lebel <plebel@o2web.ca>
2014-12-26 14:37:13 -0500

    devise authorization example on a controller with whitelist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  # devise authorization on single controller with whitelist exception without auth
+  #before_action :authenticate_user!, except: [:index]
   # use if restful resources can be loaded and checked for authorization
   #load_and_authorize_resource
 
