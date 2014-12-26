2edf2b8886e9d3c90fba105d2dc886f3558be25b
Patrice Lebel <plebel@o2web.ca>
2014-12-26 15:25:24 -0500

    simple role functionality added (strong params management is missing though)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  ROLES = %w{admin guest}
+
+  # devise/lib/devise/parameter_sanitizer.rb, line 88 : need to add 'role' for strong parameters
 end
